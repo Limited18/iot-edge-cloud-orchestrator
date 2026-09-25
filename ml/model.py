6f89ac7b4daeeb358a98773aafed598f72606e8f
VIVEK GAMANE <vivekgamane18@gmail.com>
2026-09-25 18:47:16 +0530

    Add ml/model.py

---
+++ ml/model.py
@@ -0,0 +1,5 @@
+FEATURES = [
+    "edge_cpu_percent", "edge_memory_percent", "network_latency_ms",
+    "data_size_kb", "task_complexity", "priority_level",
+    "task_type", "response_deadline_ms"
+]
